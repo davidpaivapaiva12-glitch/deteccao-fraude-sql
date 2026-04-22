7e4ff86efdca71e432eac03ef9a95b6e9e6e964e
David Paiva <davidpaiva.paiva12@gmail.com>
2026-04-22 12:11:59 -0300

    Create inserts.sql

---
+++ inserts.sql
@@ -0,0 +1,5 @@
+INSERT INTO Transacoes (Cliente, Valor, Cidade, DataTransacao) VALUES
+('João', 15000, 'São Paulo', '2026-04-01 10:00'),
+('João', 200, 'Rio de Janeiro', '2026-04-01 11:00'),
+('Maria', 300, 'São Paulo', '2026-04-02 12:00'),
+('Maria', 12000, 'São Paulo', '2026-04-02 12:05');
